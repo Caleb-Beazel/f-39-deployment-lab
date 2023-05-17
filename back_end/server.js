c7ea7052a200c452e8d4036dbd433c46503132ab
Caleb Beazel <caleb.beazel@gmail.com>
2023-05-17 16:47:39 -0600

    v4

--- back_end/server.js
+++ back_end/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.use(express.json())
 
-app.use(express.static(__dirname + '../front_end'))
+app.use(express.static(path.join(__dirname , '../front_end')))
 
 app.listen(4500, () => {
     console.log('up on 4500')
