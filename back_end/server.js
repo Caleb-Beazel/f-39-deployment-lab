79f99b0e4b20f367451441303f9ccaf049f79054
Caleb Beazel <caleb.beazel@gmail.com>
2023-05-17 16:04:34 -0600

    commit 2

--- back_end/server.js
+++ back_end/server.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(express.static(__dirname + '/front_end'))
+app.use(express.static(__dirname + '../front_end'))
 
 app.listen(4500, () => {
     console.log('up on 4500')
